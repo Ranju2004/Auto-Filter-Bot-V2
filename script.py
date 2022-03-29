8b94a109b747ae658c8560c60346efcba9ffdf66
Ranju2004 <102154261+Ranju2004@users.noreply.github.com>
2022-03-29 21:04:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,66 +1,118 @@
 class script(object):
+    START_TXT = """𝐘𝐨..𝐘𝐨.. {} 🙋, I'm Powerful Movie-Search Bot You Can Use Me As A Auto-filter Bot In Your Group ..
 
+Its Easy To Use Me; Just Add Me To Your Group As Admin, Thats All, i will Provide Movies There...🤓
 
-    START_MSG = """ <b>Hi {}
+⚠️More Help Check Help Button Below
 
-You can call this as an Auto Filter Bot if you like :D
+©️MᴀɪɴᴛᴀɪɴᴇD Bʏ  <a href=https://t.me/FHM_Movies> ғɪʟᴍʏ ʜᴀɴɢᴀᴍᴀ </a> """
+    HELP_TXT = """𝙷𝙴𝚈 {}
+𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
+    ABOUT_TXT = """╭━━━━━━━━━━━━━━━➣ 
+┣⪼ Mʏ Nᴀᴍᴇ - ℝ𝕜𝟚𝟘𝟘𝟜
+┣⪼ ғ ʜ - ""<a href=https://t.me/FHM_Movies>ғɪʟᴍʏ ʜᴀɴɢᴀᴍᴀ</a>""
+┣⪼ Gʀᴏᴜᴘ -   ""<a herf=https://t.me/Movie_requests_kannada> ClIck Here </a>""
+┣⪼ Lɪʙʀᴀʀʀʏ - 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
+┣⪼ Lᴀɴɢᴜᴀɢᴇ - 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
+┣⪼ Dᴀᴛᴀ Bᴀsᴇ - 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
+┣⪼ Bᴏᴛ Sᴇʀᴠᴇʀ -  𝙷𝙴𝚁𝙾𝙺𝚄
+┣⪼ Bᴜɪʟᴅ Pᴛᴀᴛᴜs - v1.0.1 [ 𝙱𝙴𝚃𝙰 ]
+╰━━━━━━━━━━━━━━━➣"""
+    SOURCE_TXT = """<b>NOTE:</b>
+NOTE:
+- 𝑰 𝒂𝒎 𝒏𝒐𝒕 𝒐𝒑𝒆𝒏 𝒔𝒐𝒖𝒓𝒄𝒆 𝒑𝒓𝒐𝒋𝒆𝒄𝒕. 
+- ՏOᑌᖇᑕᗴ ᑕOᗪᗴ - ""<a herf=https://t.me/newenglishfilms> F O O L </a>""
 
-This is Version 2 of Auto Filter Bot
+<b> 𝙎𝙐𝙋𝙋𝙊𝙍𝙏 </b> 
+ ""<a href= https://t.me/Movie_requests_kannada>⌠ ℝ𝕜𝟚𝟘𝟘𝟜 ⌡ </a>"""
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-Bot gives button link to files in connected channels on query !
+- Filter is the feature were users can set automated replies for a particular keyword and 𐌑᧐ᴛɦᥱr Eᥲrᴛɦ will respond whenever a keyword is found the message
 
-No need to add filters for your files or movies from now on!
+<b>NOTE:</b>
+1. ℝ𝕜𝟚𝟘𝟘𝟜 should have admin privillage.
+2. only admins can add filters in a chat.
+3. alert buttons have a limit of 64 characters.
 
-For more click <i>help</i></b>"""
+<b>Commands and Usage:</b>
+• /filter - <code>add a filter in chat</code>
+• /filters - <code>list all the filters of a chat</code>
+• /del - <code>delete a specific filter in chat</code>
+• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+    BUTTON_TXT = """Help: <b>Buttons</b>
 
+- ℝ𝕜𝟚𝟘𝟘𝟜  Supports both url and alert inline buttons.
 
-    HELP_MSG = """<b>How to use the bot??</b>
+<b>NOTE:</b>
+1. Telegram will not allows you to send buttons without any content, so content is mandatory.
+2.  ℝ𝕜𝟚𝟘𝟘𝟜 supports buttons with any telegram media type.
+3. Buttons should be properly parsed as markdown format
 
-<i>
-* Add bot to your group with admin rights.
+<b>URL buttons:</b>
+<code>[Button Text](buttonurl:https://t.me/RK2004bot)</code>
 
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
+<b>Alert buttons:</b>
+<code>[Button Text](buttonalert:This is an alert message)</code>"""
+    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
 
+<b>NOTE:</b>
+1. Make me the admin of your channel if it's private.
+2. make sure that your channel does not contains camrips, porn and fake files.
+3. Forward the last message to me with quotes.
+ I'll add all the files in that channel to my db."""
+    CONNECTION_TXT = """Help: <b>Connections</b>
 
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
+- Used to connect bot to PM for managing filters 
+- it helps to avoid spamming in groups.
 
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
+<b>NOTE:</b>
+1. Only admins can add a connection.
+2. Send <code>/connect</code> for connecting me to ur PM
 
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+<b>Commands and Usage:</b>
+• /connect  - <code>connect a particular chat to your PM</code>
+• /disconnect  - <code>disconnect from a chat</code>
+• /connections - <code>list all your connections</code>"""
+    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
+<b>NOTE:</b>
+these are the extra features of Eva Maria
 
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
+<b>Commands and Usage:</b>
+• /id - <code>get id of a specifed user.</code>
+• /info  - <code>get information about a user.</code>
+• /imdb  - <code>get the film information from IMDb source.</code>
+• /search  - <code>get the film information from various sources.</code>
+• /batch - to create link for multiple posts
+• /link - to create link for one post """
+    ADMIN_TXT = """Help: <b>Admin mods</b>
 
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
+<b>NOTE:</b>
+This module only works for my admins
 
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
-    
-⭕️<b>Creater :</b> @TroJanzHEX
-
-⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
-
+<b>Commands and Usage:</b>
+• /logs - <code>to get the rescent errors</code>
+• /stats - <code>to get status of files in db.</code>
+• /delete - <code>to delete a specific file from db.</code>
+• /users - <code>to get list of my users and ids.</code>
+• /chats - <code>to get list of the my chats and ids </code>
+• /leave  - <code>to leave from a chat.</code>
+• /disable  -  <code>do disable a chat.</code>
+• /ban  - <code>to ban a user.</code>
+• /unban  - <code>to unban a user.</code>
+• /channel - <code>to get list of total connected channels</code>
+• /broadcast - <code>to broadcast a message to all users</code>"""
+    STATUS_TXT = """✪ Total Files: <code>{}</code>
+✪ Total Users: <code>{}</code>
+✪ Total Chats: <code>{}</code>
+✪ Used Storage: <code>{}</code> 𝙼𝚒𝙱
+✪ Free Storage: <code>{}</code> 𝙼𝚒𝙱"""
+    LOG_TEXT_G = """#NewGroup
+Group = {}(<code>{}</code>)
+Total Members = <code>{}</code>
+Added By - {}
+"""
+    LOG_TEXT_P = """#NewUser
+ID - <code>{}</code>
+Name - {}
 """
